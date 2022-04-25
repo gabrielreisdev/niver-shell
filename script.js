664b613ae333726d788574872325a6369f900a81
Gabriel Reis <gabrielreistldev@gmail.com>
2022-04-24 22:32:01 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Digite seu apelido:");
+ var b=prompt("Digite a data do seu aniversario (com barras):");
+  	if(a== "shell" && b=="18/05")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("errou feio errou rude");
+  		window.location.href="index.html";
+  	}
+  	
